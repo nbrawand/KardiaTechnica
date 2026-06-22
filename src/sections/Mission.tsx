fec128e5e627487794711a39e712f5eba95844be
Nicholas Brawand <nicholasbrawand@gmail.com>
2026-06-22 06:10:42 -0700

    blank mission statement

--- src/sections/Mission.tsx
+++ src/sections/Mission.tsx
@@ -4,14 +4,10 @@ function Mission() {
   return (
     <Page>
       <h2 style={{ textAlign: 'center' }}>
-        We shape technology to help people follow Jesus.
+        Mission statement here.
       </h2>
       <p>
-        Technology shapes us. It influences how we behave, 
-        how we think, and what we value. But we shape it 
-        too — we choose what to build.  
-        We at KardiaTechnica seek to shape technology so that it 
-        helps humanity follow in the footsteps of Jesus.
+        More mission statement stuff here.
       </p>
     </Page>
   )
