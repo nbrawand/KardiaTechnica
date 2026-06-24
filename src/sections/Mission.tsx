3fcba054770e636f30e4e6c529d7be72f8815644
Nicholas Brawand <nicholasbrawand@gmail.com>
2026-06-24 07:15:11 -0700

    update mission

--- src/sections/Mission.tsx
+++ src/sections/Mission.tsx
@@ -4,10 +4,24 @@ function Mission() {
   return (
     <Page>
       <h2 style={{ textAlign: 'center' }}>
-        Mission statement here.
+        We shape the technology that shapes us.
       </h2>
       <p>
-        More mission statement stuff here.
+        Technology shapes our thoughts, behaviors, relationships, 
+        and values. In turn, we choose what technologies are 
+        created and adopted.
+      </p>
+      <p>
+        At KardiaTechnica, every technology we develop is accompanied by a model of its 
+        expected impact on individuals and society. 
+        We release these impact models alongside our 
+        technologies, making the potential benefits, 
+        risks, and tradeoffs visible to those who will build, 
+        govern, and use them.
+      </p>
+      <p>
+        We believe innovation should not only ask what can be built, 
+        but also what kind of future it will create.
       </p>
     </Page>
   )
